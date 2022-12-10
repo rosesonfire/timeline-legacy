4447c71cd1898ece1fe91a7ffc9c39105f00a7f8
Ishtiaque Khan <ishtiaq.khan@newtonx.com>
2022-12-10 22:03:47 +0600

    add nativebase

--- App.tsx
+++ App.tsx
@@ -1,115 +1,80 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ImageURISource, StyleSheet, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from 'react';
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+} from 'native-base';
+import NativeBaseIcon from './components/NativeBaseIcon';
 
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
-import IconButton from './components/IconButton';
-import CircleButton from './components/CircleButton';
-import EmojiPicker from './components/EmojiPicker';
-import EmojiList from './components/EmojiList';
-import EmojiSticker from './components/EmojiSticker';
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'dark',
+};
 
-const PlaceholderImage = require('./assets/images/background-image.png');
-
+// extend the theme
+export const theme = extendTheme({ config });
+type MyThemeType = typeof theme;
+declare module 'native-base' {
+  interface ICustomTheme extends MyThemeType {}
+}
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-  const [showAppOptions, setShowAppOptions] = useState(false);
-  const [pickedEmoji, setPickedEmoji] = useState<ImageURISource | null>(null);
-
-  const pickImageAsync = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-      setShowAppOptions(true);
-    } else {
-      alert('You did not select any image.');
-    }
-  };
-
-  const onReset = () => {
-    setShowAppOptions(false);
-  };
-
-  const onSaveImageAsync = async () => {
-    // we will implement this later
-  };
-
-  const onAddSticker = () => {
-    setIsModalVisible(true);
-  };
-
-  const onModalClose = () => {
-    setIsModalVisible(false);
-  };
-
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-          {pickedEmoji !== null ? (
-            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-          ) : null}
-        </View>
-
-        {showAppOptions ? (
-          <View style={styles.optionsContainer}>
-            <View style={styles.optionsRow}>
-              <IconButton icon="refresh" label="Reset" onPress={onReset} />
-              <CircleButton onPress={onAddSticker} />
-              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
-            </View>
-          </View>
-        ) : (
-          <View style={styles.footerContainer}>
-            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-          </View>
-        )}
-
-        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-        </EmojiPicker>
-
-        <StatusBar style="auto" />
-      </View>
-    </GestureHandlerRootView>
+    <NativeBaseProvider>
+      <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
+        <VStack space={5} alignItems="center">
+          <NativeBaseIcon />
+          <Heading size="lg">Welcome to NativeBase</Heading>
+          <HStack space={2} alignItems="center">
+            <Text>Edit</Text>
+            <Box
+              _web={{
+                _text: {
+                  fontFamily: 'monospace',
+                  fontSize: 'sm',
+                },
+              }}
+              px={2}
+              py={1}
+              _dark={{ bg: 'blueGray.800' }}
+              _light={{ bg: 'blueGray.200' }}
+            >
+              App.js
+            </Box>
+            <Text>and save to reload.</Text>
+          </HStack>
+          <Link href="https://docs.nativebase.io" isExternal>
+            <Text color="primary.500" underline fontSize={'xl'}>
+              Learn NativeBase
+            </Text>
+          </Link>
+          <ToggleDarkMode />
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
-  optionsContainer: {
-    position: 'absolute',
-    bottom: 80,
-  },
-  optionsRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-});
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === 'light'}
+        onToggle={toggleColorMode}
+        aria-label={colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'}
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
